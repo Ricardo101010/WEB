3cd5ac3fad9575490fe310cb72f033e7ce5d9653
ricardo101010 <guerrero06@gmail.com>
2024-05-02 15:30:26 -0500

    RicardoG

--- conect_db.php
+++ conect_db.php
@@ -1,3 +1,26 @@
 <?php
-    $conect = mysqli_connect('localhost', 'id21747836_admin', 'Ri16846677*', 'id21747836_llamadas');
+$servername 'localhost';
+$database 'id21747836_llamadas';
+$username = 'id21747836_admin';
+$password = 'Ri16846677*';
+
+// mysqli( host, usuario, contraseña, nombre_bd )
+$mysqli new mysqli( $servername, $username, $password, $database );
+$mysqli->set_charse( 'utf8' );
+
+//Muestra en la pagina si hay conexión
+
+if ( $mysqli ) {
+    echo '<p style='
+    display: table-cell;
+    background: #cacaca00;
+    bottom: 0;
+    position: fixed;
+    z-index: +1;
+    vertical-align: middle;
+    color: #5cbc04;
+    opacity: 25%;
+    '>0</p>';
+}
+
 ?>
